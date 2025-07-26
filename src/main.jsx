fa330900ee828582c7329da336ab3ab120ee0b63
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-07-26 16:02:56 +0530

    error fixing

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById("root")).render(
   <Context>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <ToastContainer />
-    </HashRouter>
+    </BrowserRouter>
   </Context>
 );
